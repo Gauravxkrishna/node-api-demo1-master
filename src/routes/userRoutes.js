2a3bd6add5a976cc2e30d64e7e0c5e9c9a5c4c55
Gauravxkrishna <gauravkishan17@gmail.com>
2025-09-20 22:54:07 +0530

    new image aws

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/", userController.createUser);
+// Multer setup (store in memory for direct upload to S3)
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/", upload.single("image"), userController.createUser);
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUser);
 router.put("/:id", userController.updateUser);
